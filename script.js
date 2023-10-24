b4061bd7a19e3470c5c06faeb28b661dfaa40e11
Sonal Antony <56635721+sonalantony@users.noreply.github.com>
2023-10-24 21:14:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
-// You would need to implement the code for fetching and displaying data from Google Docs here.
+document.addEventListener("DOMContentLoaded", function () {
+  // Define the Google Docs document URLs you want to fetch.
+  const docUrls = [
+    "https://docs.google.com/document/d/14fimPIbEgRQl6c7wDTRq_A_IUJTQhVN8S9zw0KKutPY/export?format=txt",
+    "https://docs.google.com/document/d/1PMPoS5wp5xaLiP3wfwJ4xV3QRcV2knPStd2-9euRwu4/export?format=txt",
+    "https://docs.google.com/document/d/11T54LH0-l1XOW_vgdG_WwvgoDsveFTmxU7X7enBMS1Q/export?format=txt"
+  ];
 
-// For instance, you could use the Google Docs API to fetch the content from the specified URLs
-// and parse the data to display it on your webpage.
-
-// Below is a simplified example of how you might fetch data from Google Docs. In a real project, you
-// would need to set up OAuth 2.0 authentication and access control, which is beyond the scope of this example.
-
-// Sample Google Docs API request using the Fetch API:
-const googleDocsUrl = "https://docs.google.com/document/d/14fimPIbEgRQl6c7wDTRq_A_IUJTQhVN8S9zw0KKutPY/export?format=txt"; // Replace with your actual URL
-
-fetch(googleDocsUrl)
-    .then(response => response.text())
-    .then(data => {
-        // Process the data and display it on the webpage
-        displayProgressData(data);
-    })
-    .catch(error => {
-        console.error("Error fetching data from Google Docs: " + error);
-    });
-
-function displayProgressData(data) {
-    // Parse and display the data on your webpage as needed.
+  // Function to fetch and display progress data from Google Docs documents.
+  async function fetchAndDisplayProgressData() {
     const progressContainer = document.getElementById("progress-container");
-    progressContainer.innerHTML = data; // Display the data in the progress container.
-}
+    progressContainer.innerHTML = "Loading..."; // Display loading message.
+
+    for (const docUrl of docUrls) {
+      try {
+        const response = await fetch(docUrl);
+        if (response.ok) {
+          const data = await response.text();
+          displayProgressData(progressContainer, data);
+        } else {
+          console.error(`Failed to fetch data from ${docUrl}`);
+        }
+      } catch (error) {
+        console.error(`Error fetching data from ${docUrl}: ${error}`);
+      }
+    }
+  }
+
+  // Function to display progress data on the webpage.
+  function displayProgressData(container, data) {
+    // Parse and format the data as needed.
+    // For example, you can create HTML elements to display the data.
+    const progressDataElement = document.createElement("div");
+    progressDataElement.textContent = data;
+    container.appendChild(progressDataElement);
+  }
+
+  // Call the function to fetch and display progress data when the page loads.
+  fetchAndDisplayProgressData();
+});
